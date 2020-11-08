7665ab7f53c1efec21ef5fea101126a7415b84ba
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2020-11-08 14:59:40 +0100

    Refactoring

--- examples/example.py
+++ examples/example.py
@@ -34,9 +34,9 @@ async def main():
         # Connect to the controller. This call must be done in the loop.
         await gpib_device.connect()
         version = await gpib_device.version()
-        print("Controller version: ", version)
+        print('Controller version: ', version)
     except (ConnectionError, ConnectionRefusedError):
-        print("Could not connect to remote target. Connection refused. Is the device connected?")
+        print('Could not connect to remote target. Connection refused. Is the device connected?')
     finally:
         # Disconnect from the GPIB controller. We may safely call diconnect() on a non-connected gpib device, that
         # means in case of a connection error
