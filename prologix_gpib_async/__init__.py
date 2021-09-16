07fc445be5bfa3d2b565588709d8fd503ab908e5
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2021-09-16 17:38:05 +0200

    Added convenience imports to __init__.py

--- prologix_gpib_async/__init__.py
+++ prologix_gpib_async/__init__.py
@@ -1,2 +1,5 @@
 # pylint: disable=missing-module-docstring
+from .prologix_gpib_async import DeviceMode, EosMode, RqsMask, AsyncPrologixGpibEthernetController, AsyncPrologixGpibEthernetDevice
 from ._version import __version__
+
+version = __version__
